137d4769b53a035b8f3476f56d507caec3a61bbc
IgorBayerl <dev.igorbayerl@gmail.com>
2024-09-06 13:36:22 -0300

    hotfix: remove unused import

--- client/src/components/organisms/ProductAddCsv.tsx
+++ client/src/components/organisms/ProductAddCsv.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
